e56771c2ed71f328328c5f52c0a3f868802acc1c
kwabena-aboah <obed30mintah@yahoo.com>
2026-08-26 16:35:36 +0000

    Upload to vercel

--- auction_platform/settings/base.py
+++ auction_platform/settings/base.py
@@ -213,14 +213,57 @@ CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 
 # Email
-EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend')
-EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
-EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', default='True', cast=bool)
-EMAIL_USE_SSL = config('EMAIL_USE_SSL', default='False', cast=bool)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='MachineAuction <noreply@ghanahammer.com>')
+# Email Configuration
+
+EMAIL_BACKEND = config(
+    'EMAIL_BACKEND',
+    default='django.core.mail.backends.smtp.EmailBackend'
+)
+
+EMAIL_HOST = config(
+    'EMAIL_HOST',
+    default='smtp.gmail.com'
+)
+
+EMAIL_PORT = config(
+    'EMAIL_PORT',
+    default='587'
+)
+
+# Prevent empty EMAIL_PORT from crashing the application
+EMAIL_PORT = int(EMAIL_PORT or 587)
+
+EMAIL_HOST_USER = config(
+    'EMAIL_HOST_USER',
+    default=''
+)
+
+EMAIL_HOST_PASSWORD = config(
+    'EMAIL_HOST_PASSWORD',
+    default=''
+)
+
+EMAIL_USE_TLS = config(
+    'EMAIL_USE_TLS',
+    default=True,
+    cast=bool
+)
+
+EMAIL_USE_SSL = config(
+    'EMAIL_USE_SSL',
+    default=False,
+    cast=bool
+)
+
+DEFAULT_FROM_EMAIL = config(
+    'DEFAULT_FROM_EMAIL',
+    default=EMAIL_HOST_USER
+)
+
+SERVER_EMAIL = config(
+    'SERVER_EMAIL',
+    default=DEFAULT_FROM_EMAIL
+)
 
 # Paystack
 PAYSTACK_PUBLIC_KEY = config('PAYSTACK_PUBLIC_KEY', default='pk_test_xxxx')
